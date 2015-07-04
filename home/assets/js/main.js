e7b3f3d8a1f108fd4787cd349277008db8c070c8
Nebojsa Petrovic <nebspetrovic@gmail.com>
2015-07-04 10:37:33 -0700

    Add SurFMan and WaveLab to projects

--- home/assets/js/main.js
+++ home/assets/js/main.js
@@ -16,14 +16,6 @@
 
 	};
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1800px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
-	});
-
 	$(function() {
 
 		var $window = $(window),
@@ -37,62 +29,10 @@
 				$body.removeClass('is-loading');
 			});
 
-		// Touch?
-			if (skel.vars.mobile) {
-
-				// Turn on touch mode.
-					$body.addClass('is-touch');
-
-				// Height fix (mostly for iOS).
-					window.setTimeout(function() {
-						$window.scrollTop($window.scrollTop() + 1);
-					}, 0);
-
-			}
-
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
 		// Header.
-
-			// Parallax background.
-
-				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-					if (skel.vars.browser == 'ie'
-					||	skel.vars.mobile)
-						settings.parallax = false;
-
-				if (settings.parallax) {
-
-					skel.on('change', function() {
-
-						if (skel.breakpoint('medium').active) {
-
-							$window.off('scroll.strata_parallax');
-							$header.css('background-position', 'top left, center center');
-
-						}
-						else {
-
-							$header.css('background-position', 'left 0px');
-
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
-
-						}
-
-					});
-
-				}
 
 		// Main Sections: Two.
 
@@ -110,7 +50,6 @@
 						usePopupDefaultStyling: false,
 						usePopupEasyClose: false,
 						usePopupNav: true,
-						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
 					});
 
 				});
